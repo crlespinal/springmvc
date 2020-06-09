ea540e77c85a8efce9fc046d16e8022b0fb19e02
Carlos_E <crlespinal@gmail.com>
2020-06-09 13:47:20 -0400

    Init commit

---
+++ src/main/java/com/example/somefront/controllers/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example.somefront.controllers;
+
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+            Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("time", LocalDate.now().toString());
+        return "greeting";
+    }
+}
